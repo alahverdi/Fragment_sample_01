32498607e811622c2d75988f61f05bb5d15b775c
alroid <al.allahverdi@gmail.com>
2021-04-16 14:31:47 +0430

    setup adapter and rv A

--- app/src/main/java/com/alroid/fragmentsample/fragment/FragmentA.java
+++ app/src/main/java/com/alroid/fragmentsample/fragment/FragmentA.java
@@ -4,20 +4,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.alroid.fragmentsample.DataGenerator;
 import com.alroid.fragmentsample.R;
+import com.alroid.fragmentsample.adapter.AdapterItem;
+import com.alroid.fragmentsample.entity.Item;
 
-public class FragmentA extends Fragment {
-
+public class FragmentA extends Fragment implements AdapterItem.selectedItem {
+    RecyclerView rv_a;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_a, null);
 
+        rv_a = view.findViewById(R.id.rv_a);
+        setupRecyclerViewItemsA();
+
         return view;
+    }
+
+    private void setupRecyclerViewItemsA() {
+        AdapterItem adapter = new AdapterItem(getActivity(), DataGenerator.getItemsA(getActivity()), this);
+        rv_a.setLayoutManager(new GridLayoutManager(getActivity(), 2, RecyclerView.HORIZONTAL, false));
+        rv_a.setAdapter(adapter);
+    }
+
+    @Override
+    public void selectedItem(Item item) {
+        Toast.makeText(getActivity(), "hi", Toast.LENGTH_SHORT).show();
     }
 }
