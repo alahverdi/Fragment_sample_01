1f0917c4a4580d4225d6bb6a295bba37c2ad23cb
alroid <al.allahverdi@gmail.com>
2021-04-17 15:06:12 +0430

    add final lotties

--- app/src/main/java/com/alroid/fragmentsample/fragment/FragmentResult.java
+++ app/src/main/java/com/alroid/fragmentsample/fragment/FragmentResult.java
@@ -5,19 +5,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
 import com.alroid.fragmentsample.Const;
 import com.alroid.fragmentsample.R;
 
 public class FragmentResult extends Fragment {
 
-    ImageView iv_b, iv_c;
-    LottieAnimationView iv_a;
+    LottieAnimationView iv_a, iv_c, iv_b;
 
     public static FragmentResult newInstance(String reset) {
         FragmentResult fragmentResult = new FragmentResult();
@@ -32,29 +33,33 @@ public class FragmentResult extends Fragment {
     public void setIdA(int id_a) {
         switch (id_a) {
             case 0:
-                iv_a.setAnimation(R.raw.sample_burger);
+                iv_a.setAnimation(R.raw.bread1_lottie);
                 iv_a.playAnimation();
+                iv_a.loop(false);
                 break;
             case 1:
-                iv_a.setImageResource(R.drawable.ic_android);
+                iv_a.setAnimation(R.raw.bread2_lottie);
+                iv_a.playAnimation();
+                iv_a.loop(false);
                 break;
             case 2:
-                iv_a.setImageResource(R.drawable.ic_baseline_cast_24);
-                break;
+                iv_a.setAnimation(R.raw.bread3_lottie);
+                iv_a.playAnimation();
+                iv_a.loop(false);
             case 3:
-                iv_a.setImageResource(R.drawable.ic_launcher_background);
+                iv_a.setAnimation(R.raw.bread1_lottie);
+                iv_a.playAnimation();
+                iv_a.loop(false);
                 break;
             case 4:
-                iv_a.setImageResource(R.drawable.ic_android);
+                iv_a.setAnimation(R.raw.bread2_lottie);
+                iv_a.playAnimation();
+                iv_a.loop(false);
                 break;
             case 5:
-                iv_a.setImageResource(R.drawable.ic_launcher_background);
-                break;
-            case 6:
-                iv_a.setImageResource(R.drawable.ic_android);
-                break;
-            case 7:
-                iv_a.setImageResource(R.drawable.ic_baseline_cast_24);
+                iv_a.setAnimation(R.raw.bread3_lottie);
+                iv_a.playAnimation();
+                iv_a.loop(false);
                 break;
         }
     }
@@ -62,28 +67,34 @@ public class FragmentResult extends Fragment {
     public void setIdB(int id_b) {
         switch (id_b) {
             case 0:
-                iv_b.setImageResource(R.drawable.ic_launcher_background);
+                iv_b.setAnimation(R.raw.protein1_lottie);
+                iv_b.playAnimation();
+                iv_b.loop(false);
                 break;
             case 1:
-                iv_b.setImageResource(R.drawable.ic_android);
+                iv_b.setAnimation(R.raw.protein2_lottie);
+                iv_b.playAnimation();
+                iv_b.loop(false);
                 break;
             case 2:
-                iv_b.setImageResource(R.drawable.ic_baseline_cast_24);
+                iv_b.setAnimation(R.raw.protein3_lottie);
+                iv_b.playAnimation();
+                iv_b.loop(false);
                 break;
             case 3:
-                iv_b.setImageResource(R.drawable.ic_launcher_background);
+                iv_b.setAnimation(R.raw.protein1_lottie);
+                iv_b.playAnimation();
+                iv_b.loop(false);
                 break;
             case 4:
-                iv_b.setImageResource(R.drawable.ic_android);
+                iv_b.setAnimation(R.raw.protein2_lottie);
+                iv_b.playAnimation();
+                iv_b.loop(false);
                 break;
             case 5:
-                iv_b.setImageResource(R.drawable.ic_launcher_background);
-                break;
-            case 6:
-                iv_b.setImageResource(R.drawable.ic_android);
-                break;
-            case 7:
-                iv_b.setImageResource(R.drawable.ic_baseline_cast_24);
+                iv_b.setAnimation(R.raw.protein3_lottie);
+                iv_b.playAnimation();
+                iv_b.loop(false);
                 break;
         }
     }
@@ -91,38 +102,50 @@ public class FragmentResult extends Fragment {
     public void setIdC(int id_c) {
         switch (id_c) {
             case 0:
-                iv_c.setImageResource(R.drawable.ic_launcher_background);
+                iv_c.setAnimation(R.raw.lettuce1_lottie);
+                iv_c.playAnimation();
+                iv_c.loop(false);
                 break;
             case 1:
-                iv_c.setImageResource(R.drawable.ic_android);
+                iv_c.setAnimation(R.raw.lettuce2_lottie);
+                iv_c.playAnimation();
+                iv_c.loop(false);
                 break;
             case 2:
-                iv_c.setImageResource(R.drawable.ic_baseline_cast_24);
+                iv_c.setAnimation(R.raw.lettuce3_lottie);
+                iv_c.playAnimation();
+                iv_c.loop(false);
                 break;
             case 3:
-                iv_c.setImageResource(R.drawable.ic_launcher_background);
+                iv_c.setAnimation(R.raw.lettuce1_lottie);
+                iv_c.playAnimation();
+                iv_c.loop(false);
                 break;
             case 4:
-                iv_c.setImageResource(R.drawable.ic_android);
+                iv_c.setAnimation(R.raw.lettuce2_lottie);
+                iv_c.playAnimation();
+                iv_c.loop(false);
                 break;
             case 5:
-                iv_c.setImageResource(R.drawable.ic_launcher_background);
-                break;
-            case 6:
-                iv_c.setImageResource(R.drawable.ic_android);
-                break;
-            case 7:
-                iv_c.setImageResource(R.drawable.ic_baseline_cast_24);
+                iv_c.setAnimation(R.raw.lettuce3_lottie);
+                iv_c.playAnimation();
+                iv_c.loop(false);
                 break;
         }
     }
 
     public void resetFragmentResult() {
-        iv_a.setAnimation(R.raw.sample_burger);
-        //iv_a.setImageResource(R.drawable.ic_launcher_background);
-        iv_b.setImageResource(R.drawable.ic_launcher_background);
-        iv_c.setImageResource(R.drawable.ic_launcher_background);
+        iv_a.setAnimation(R.raw.reset_iv_a);
+        iv_a.playAnimation();
+        iv_a.loop(true);
 
+        iv_b.setAnimation(R.raw.reset_iv_b);
+        iv_b.playAnimation();
+        iv_b.loop(true);
+
+        iv_c.setAnimation(R.raw.reset_iv_c);
+        iv_c.playAnimation();
+        iv_c.loop(true);
     }
 
     @Nullable
